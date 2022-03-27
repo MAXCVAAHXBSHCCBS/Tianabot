0fe17e53a76a76068929e5a2478538ac5a3d0120
MAXCVAAHXBSHCCBS <101635026+MAXCVAAHXBSHCCBS@users.noreply.github.com>
2022-03-27 11:39:59 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE ＴＨＥܔＳＨＥＩＬＤ ★
-:: Enables virtual env mode and then starts Tiana
+:: Enables virtual env mode and then starts ＴＨＥܔＳＨＥＩＬＤ ★
 env\scripts\activate.bat && py -m ＴＨＥܔＳＨＥＩＬＤ ★
